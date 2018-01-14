c85733c888f5dc929f2917e6cddf59c1d1fdd137
alexis-ber <alexisbernier96@gmail.com>
2018-01-14 21:54:28 +0100

    ajout des fichier importants

---
+++ TP Keras_graph.py
@@ -0,0 +1,90 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+# dimensions of our images.
+img_width, img_height = 224,224
+
+train_data_dir = 'traingraph'
+validation_data_dir = 'Validationgraph'
+nb_train_samples = 2000
+nb_validation_samples = 800
+epochs = 2
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape = input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D((2,2), strides=2))
+
+model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D((2,2), strides=2))
+
+model.add(Conv2D(256, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D((2,2), strides=2))
+
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D((2,2), strides=2))
+
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D((2,2), strides=2))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save_weights('poids_notre_vgg16.h5')
+
+model.save('model_notre_vgg16.h5')
